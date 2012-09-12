c106b12ff3996af2e4eea6f7bcd7a59840cf0ce1
hkiaipc <hkiaipc@163.com>
2012-09-12 16:10:34 +0800

    ...

---
+++ 8.Src/fnbx/fnbx/UCDateTimePicker.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace fnbx
+{
+    partial class UCDateTimePicker
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.dtpTime = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(0, 0);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(114, 21);
+            this.dtpDate.TabIndex = 0;
+            // 
+            // dtpTime
+            // 
+            this.dtpTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpTime.Location = new System.Drawing.Point(117, 0);
+            this.dtpTime.Name = "dtpTime";
+            this.dtpTime.ShowUpDown = true;
+            this.dtpTime.Size = new System.Drawing.Size(80, 21);
+            this.dtpTime.TabIndex = 1;
+            this.dtpTime.Value = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            // 
+            // UCDateTimePicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dtpTime);
+            this.Controls.Add(this.dtpDate);
+            this.Name = "UCDateTimePicker";
+            this.Size = new System.Drawing.Size(200, 21);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.DateTimePicker dtpTime;
+    }
+}
---
+++ 8.Src/fnbx/fnbx/UCDateTimePicker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace fnbx
+{
+    public partial class UCDateTimePicker : UserControl
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public UCDateTimePicker()
+        {
+            InitializeComponent();
+
+            this.Value = DateTime.Now;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime Value
+        {
+            get { return dtpDate.Value.Date + TimeSpan.FromSeconds((int)dtpTime.Value.TimeOfDay.TotalSeconds); }
+            set
+            {
+                this.dtpDate.Value = value.Date;
+                this.dtpTime.Value = value;
+            }
+        }
+    }
+}
