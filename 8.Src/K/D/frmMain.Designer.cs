d1a035d36a33849b68e02f0339bc71a1eaac9373
hkiaipc <hkiaipc@163.com>
2012-12-25 11:24:52 +0800

    add d project(tmdata insert tool)

---
+++ 8.Src/K/D/frmMain.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace D
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbGroup = new System.Windows.Forms.ComboBox();
+            this.cmbPerson = new System.Windows.Forms.ComboBox();
+            this.dtpInsertDT = new System.Windows.Forms.DateTimePicker();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbStation = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpQuery = new System.Windows.Forms.DateTimePicker();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmbGroup
+            // 
+            this.cmbGroup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGroup.FormattingEnabled = true;
+            this.cmbGroup.Location = new System.Drawing.Point(104, 20);
+            this.cmbGroup.Name = "cmbGroup";
+            this.cmbGroup.Size = new System.Drawing.Size(200, 20);
+            this.cmbGroup.TabIndex = 0;
+            this.cmbGroup.SelectedIndexChanged += new System.EventHandler(this.cmbGroup_SelectedIndexChanged);
+            // 
+            // cmbPerson
+            // 
+            this.cmbPerson.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPerson.FormattingEnabled = true;
+            this.cmbPerson.Location = new System.Drawing.Point(104, 46);
+            this.cmbPerson.Name = "cmbPerson";
+            this.cmbPerson.Size = new System.Drawing.Size(200, 20);
+            this.cmbPerson.TabIndex = 1;
+            // 
+            // dtpInsertDT
+            // 
+            this.dtpInsertDT.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dtpInsertDT.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpInsertDT.Location = new System.Drawing.Point(100, 19);
+            this.dtpInsertDT.Name = "dtpInsertDT";
+            this.dtpInsertDT.Size = new System.Drawing.Size(200, 21);
+            this.dtpInsertDT.TabIndex = 2;
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(229, 99);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 3;
+            this.btnQuery.Text = "查询";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "部门:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "人员:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "时间:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(334, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(438, 247);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "站点:";
+            // 
+            // cmbStation
+            // 
+            this.cmbStation.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbStation.FormattingEnabled = true;
+            this.cmbStation.Location = new System.Drawing.Point(100, 46);
+            this.cmbStation.Name = "cmbStation";
+            this.cmbStation.Size = new System.Drawing.Size(200, 20);
+            this.cmbStation.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "时间:";
+            // 
+            // dtpQuery
+            // 
+            this.dtpQuery.CustomFormat = "yyyy-MM";
+            this.dtpQuery.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpQuery.Location = new System.Drawing.Point(104, 72);
+            this.dtpQuery.Name = "dtpQuery";
+            this.dtpQuery.Size = new System.Drawing.Size(200, 21);
+            this.dtpQuery.TabIndex = 10;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(231, 72);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "添加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cmbStation);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.dtpInsertDT);
+            this.groupBox1.Location = new System.Drawing.Point(12, 143);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(316, 103);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.dtpQuery);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnQuery);
+            this.groupBox2.Controls.Add(this.cmbPerson);
+            this.groupBox2.Controls.Add(this.cmbGroup);
+            this.groupBox2.Location = new System.Drawing.Point(14, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(314, 133);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 263);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbGroup;
+        private System.Windows.Forms.ComboBox cmbPerson;
+        private System.Windows.Forms.DateTimePicker dtpInsertDT;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbStation;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpQuery;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ 8.Src/K/D/frmMain.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using KDB;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace D
+{
+    public partial class frmMain : Form
+    {
+        private DB _db;
+        public frmMain()
+        {
+            InitializeComponent();
+
+            _db = DBFactory.GetDB();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.dtpQuery.Value = DateTime.Now.Date ;
+            FillGroup(_db);
+            FillStation(_db);
+        }
+
+        private void FillStation(DB db)
+        {
+            var r = from q in db.tblDevice
+                    where q.DeviceType == "xgdevice"
+                    orderby q.tblStation.StationName
+                    select new { q.tblStation.StationName, q.DeviceID };
+
+            this.cmbStation.DisplayMember = "StationName";
+            this.cmbStation.ValueMember = "DeviceID";
+            this.cmbStation.DataSource = r;
+        }
+
+        private void FillGroup(DB db)
+        {
+            // fill group
+            //
+            var r = from q in db.tblGroup
+                    select q;
+
+            this.cmbGroup.DisplayMember = "GroupName";
+            this.cmbGroup.ValueMember = "GroupID";
+            this.cmbGroup.DataSource = r;
+        }
+
+        private void cmbGroup_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.cmbGroup.SelectedIndex >= 0)
+            {
+                int groupID = (int)this.cmbGroup.SelectedValue;
+                FillPerson(_db, groupID);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="_db"></param>
+        /// <param name="groupID"></param>
+        private void FillPerson(DB db, int groupID)
+        {
+            var r = from q in db.tblPerson
+                    where q.GroupID == groupID
+                    orderby q.PersonName
+                    select q;
+
+            this.cmbPerson.DisplayMember = "PersonName";
+            this.cmbPerson.ValueMember = "PersonID";
+            this.cmbPerson.DataSource = r;
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            if ( this.cmbPerson.SelectedIndex < 0 )
+            {
+                return;
+            }
+
+            DB db = DBFactory.GetDB ();
+            int personID = (int)this.cmbPerson.SelectedValue;
+            int tmid = GetTMID(db, personID);
+            if (tmid <= 0)
+            {
+                return;
+            }
+
+            var r = from q in db.tblTmData
+                    where q.tblTM.TmID == tmid && q.TmDataDT >= MonthFirst() && q.TmDataDT < MonthLast()
+                    orderby q.TmDataDT
+                    select new { 时间=q.TmDataDT, 站=q.tblDevice.tblStation.StationName, 人=q.tblTM.tblPerson[0].PersonName };
+
+            this.dataGridView1.DataSource = r;
+        }
+
+
+        private DateTime MonthFirst()
+        {
+             DateTime dt = this.dtpQuery.Value;
+             return new DateTime(dt.Year, dt.Month, 1);
+        }
+
+        private DateTime MonthLast()
+        {
+            DateTime dt = this.dtpQuery.Value;
+            int days = DateTime.DaysInMonth(dt.Year, dt.Month);
+            return dt.Date + TimeSpan.FromDays(days);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="db"></param>
+        /// <returns></returns>
+        private int GetTMID(DB db, int personID)
+        {
+            var r = from q in db.tblPerson
+                    where q.PersonID == personID
+                    select q;
+
+            tblPerson p = r.First();
+            if (p.tblTM != null)
+            {
+                return p.tblTM.TmID;
+            }
+            return -1;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (this.cmbStation.SelectedIndex < 0)
+            {
+                return;
+            }
+            if (this.cmbPerson.SelectedIndex < 0)
+            {
+                return;
+            }
+
+            string text = string.Format("添加 {0}, {1}, {2} ?", cmbPerson.Text, cmbStation.Text, this.dtpInsertDT.Value);
+            if (MessageBox.Show(text, "cap", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                return;
+            }
+            DB db = DBFactory.GetDB();
+
+            tblTmData tmdata = new tblTmData();
+            tmdata.tblDevice = db.tblDevice.Single(c => c.DeviceID == GetSelectedDeviceID());
+            int tmid = GetSelectedTmID(db);
+            tmdata.tblTM = db.tblTM.Single(c => c.TmID == tmid);
+            tmdata.TmDataDT = this.dtpInsertDT.Value;
+
+            db.tblTmData.InsertOnSubmit(tmdata);
+            db.SubmitChanges();
+
+            MessageBox.Show("添加成功");
+        }
+
+        private int GetSelectedTmID(DB db)
+        {
+            int personID = (int)this.cmbPerson.SelectedValue;
+            return GetTMID(db, personID);
+        }
+
+        private int GetSelectedDeviceID()
+        {
+            return (int)this.cmbStation.SelectedValue;
+        }
+    }
+}
